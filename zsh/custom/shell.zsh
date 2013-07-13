0517664db970c757be3e32a433e812e1bc67b6ee
David Vuong <david.vuong256@gmail.com>
2013-07-13 22:10:26 +1000

    Included install script.
    - Removed symlinks.zsh
    - Updated zsh_nocorrect to include pip3 and python3.

--- zsh/custom/shell.zsh
+++ zsh/custom/shell.zsh
@@ -1,13 +1,7 @@
 # Set Vim as the default terminal editor
 export EDITOR="vim"
 
-# If homebrew is installed then we want to use brews instead of defaults.
-if [ -d "/usr/local/Cellar" ]; then;
-    export PATH=/usr/local/Cellar:$PATH
-fi
-
 # Prepends "nocorrect" to avoid zsh's bs correct feature.
-#
 # zsh: correct 'x' to 'y'[nyae]?
 if [ -f ~/zsh_nocorrect ]; then
     while read -r COMMAND; do
