0bc83f13fbb88641c3d67329b84b10ae334191a1
David Vuong <david.vuong256@gmail.com>
2013-08-11 17:02:50 +1000

    Adding virtualenvwrapper

--- zsh/custom/shell.zsh
+++ zsh/custom/shell.zsh
@@ -1,7 +1,13 @@
 # Set Vim as the default terminal editor
 export EDITOR="vim"
-
 export PATH="/Users/davidvuong/Documents/rails_one_click/ruby/bin":$PATH
+
+# Shell virtualenvwrapper startup.
+if [ -f /usr/local/bin/virtualenvwrapper.sh ]; then
+    export WORKON_HOME=$HOME/.virtualenvs
+    export PROJECT_HOME=$HOME/Devel
+    source /usr/local/bin/virtualenvwrapper.sh
+fi
 
 # Prepends "nocorrect" to avoid zsh's bs correct feature.
 # zsh: correct 'x' to 'y'[nyae]?
