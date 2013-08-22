c9b0d434e54abac1b3decb29430ed95eaf8ad1cc
David Vuong <david.vuong256@gmail.com>
2013-08-22 16:48:44 +1000

    Added some more aliases.

--- zsh/custom/aliases.zsh
+++ zsh/custom/aliases.zsh
@@ -2,6 +2,10 @@ alias l="ls -l"
 alias ll="ls -la"
 
 alias cd..="cd .."
+alias ..3="../../../"
+
+alias h='history'
+alias j='jobs -l'
 
 # Use MacVim if we're on a Mac.
 if [ "$(uname)" = "Darwin" ]; then;
