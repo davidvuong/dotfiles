00da548d0ef80719936fcf264fed08823a029ff0
David Vuong <david.vuong256@gmail.com>
2018-01-03 22:35:51 +1100

    rm(scripts/python): stop using git-up

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -8,4 +8,3 @@ brew install python
 sudo pip2 install --ignore-installed virtualenv
 sudo pip2 install --ignore-installed virtualenvwrapper
 sudo pip2 install --ignore-installed flake8
-sudo pip2 install --ignore-installed git-up
