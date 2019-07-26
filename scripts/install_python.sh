5d4965d22ade02de47430029aaf7495376b3952d
David Vuong <davidvuong@users.noreply.github.com>
2019-07-26 15:53:58 +1000

    feat(scripts): stop using pip2 and just use pip

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 
-set -eu
-
-brew install python3
-brew install python
-
-sudo pip2 install --ignore-installed virtualenv
-sudo pip2 install --ignore-installed virtualenvwrapper
-sudo pip2 install --ignore-installed flake8
+sudo pip install --ignore-installed virtualenv
+sudo pip install --ignore-installed virtualenvwrapper
+sudo pip install --ignore-installed flake8
