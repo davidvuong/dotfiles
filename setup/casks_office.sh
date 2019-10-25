bf3554a9a3ae6f273a0a7352970bba79834e3af6
David Vuong <davidvuong@users.noreply.github.com>
2019-10-25 15:50:54 +1100

    feat(setup): add a cutdown version of casks for office

---
+++ setup/casks_office.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -eu
+
+brew tap caskroom/versions
+
+brew cask install google-chrome
+brew cask install firefox
+
+brew cask install slack
+brew cask install alfred
+brew cask install spotify
+brew cask install caffeine
+
+brew cask install iterm2
+
+brew cask install virtualbox
+brew cask install docker
+brew cask install docker-toolbox
+
+brew cask install visual-studio-code
+brew cask install spectacle
+
+brew cleanup
