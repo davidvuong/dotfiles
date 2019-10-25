d5c7d9b66d54eb26d4a25dbc2971b46a1bcc5935
David Vuong <davidvuong@users.noreply.github.com>
2019-10-25 16:55:12 +1100

    rm(casks): drop virtualbox

--- setup/casks_office.sh
+++ setup/casks_office.sh
@@ -11,14 +11,11 @@ brew cask install slack
 brew cask install alfred
 brew cask install spotify
 brew cask install caffeine
-
 brew cask install iterm2
-
-brew cask install virtualbox
-brew cask install docker
-brew cask install docker-toolbox
-
 brew cask install visual-studio-code
 brew cask install spectacle
+
+brew cask install docker
+brew cask install docker-toolbox
 
 brew cleanup
