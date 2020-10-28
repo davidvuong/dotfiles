4f53cda2ed0122c7633d558218ba1d3dbdfacb0f
David Vuong <david.vuong@voltron.studio>
2020-10-28 12:09:45 +1100

    feat: Include Python's poetry pkg manager in install_python

--- scripts/install_python.sh
+++ scripts/install_python.sh
@@ -2,3 +2,5 @@
 
 sudo pip3 install --ignore-installed virtualenv
 sudo pip3 install --ignore-installed virtualenvwrapper
+
+curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python
