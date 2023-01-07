d179bf3118be26b8f7708584b398078e90d13bb3
rasputincat.eth <davidvuong@users.noreply.github.com>
2023-01-07 19:50:34 +1100

    rm: drop a few casks no longer used

--- setup/casks.sh
+++ setup/casks.sh
@@ -5,11 +5,8 @@ set -eu
 brew tap homebrew/cask-versions
 
 brew install --cask google-chrome
-brew install --cask firefox
 
-brew install --cask slack
 brew install --cask vlc
-brew install --cask qbittorrent
 brew install --cask appcleaner
 brew install --cask alfred
 brew install --cask dropbox
@@ -18,8 +15,6 @@ brew install --cask caffeine
 brew install --cask docker
 
 brew install --cask iterm2
-brew install --cask pgadmin4
-brew install --cask mysqlworkbench
 brew install --cask lens
 
 brew install --cask visual-studio-code
