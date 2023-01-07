481ec31c2e1a3437a7e7c472a1775856ce4349ed
rasputincat.eth <davidvuong@users.noreply.github.com>
2023-01-07 19:52:53 +1100

    feat: brew install discord cask

--- setup/casks.sh
+++ setup/casks.sh
@@ -5,7 +5,7 @@ set -eu
 brew tap homebrew/cask-versions
 
 brew install --cask google-chrome
-
+brew install --cask discord
 brew install --cask vlc
 brew install --cask appcleaner
 brew install --cask alfred
@@ -13,10 +13,8 @@ brew install --cask dropbox
 brew install --cask spotify
 brew install --cask caffeine
 brew install --cask docker
-
 brew install --cask iterm2
 brew install --cask lens
-
 brew install --cask visual-studio-code
 brew install --cask spectacle
 
