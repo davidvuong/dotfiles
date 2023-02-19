07317f0d360c2a829d1d12073ca9eb0e8ff9bf51
David Vuong <david.vuong256@gmail.com>
2023-02-20 10:51:47 +1100

    feat: Add signal/whatsapp rm caffeine

--- setup/casks.sh
+++ setup/casks.sh
@@ -11,12 +11,13 @@ brew install --cask appcleaner
 brew install --cask alfred
 brew install --cask dropbox
 brew install --cask spotify
-brew install --cask caffeine
 brew install --cask docker
 brew install --cask iterm2
 brew install --cask lens
 brew install --cask visual-studio-code
 brew install --cask spectacle
 brew install --cask notion
+brew install --cask signal
+brew install --cask whatsapp
 
 brew cleanup
